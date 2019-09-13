7b413b770cb968e59943ae1d18e5d5fe6bd6d053
Steffen Fortner <fortner.steffen@web.de>
2019-09-13 14:06:57 +0200

    SaveActions & groundwork

---
+++ skip_list.py
@@ -0,0 +1,25 @@
+class SkipListNode():
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __le__(self, other):
+        pass
+
+    def __lt__(self, other):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __ge__(self, other):
+        pass
+
+    def __gt__(self, other):
+        pass
+
+    def __ne__(self, other):
+        pass
