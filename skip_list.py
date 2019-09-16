3c11519dbde30d86fc05060c2b2f3fb9e78d5e09
Steffen Fortner <fortner.steffen@web.de>
2019-09-16 09:43:20 +0200

    Simple Linked List

--- skip_list.py
+++ skip_list.py
@@ -21,6 +21,47 @@ class LinkedList():
     def __init__(self, head=None):
         self.head = head
 
+    def insert(self, data):
+        new_node = LinkedListNode(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+    def size(self):
+        current = self.head
+        counter = 0
+        while current:
+            counter += 1
+            current = current.get_next()
+        return counter
+
+    def find(self, data):
+        current = self.head
+        found = False
+        while current and not found:
+            if current.get_data == data:
+                return current
+            else:
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Data not found!")
+
+    def delete(self, data):
+        current = self.head
+        previous = None
+        found = False
+        while current and not found:
+            if current.get_data == data:
+                found = True
+            else:
+                current = current.get_next()
+                previous = current
+        if current is None:
+            raise ValueError("Data not found!")
+        if previous is None:
+            raise ValueError("Data not found!")
+        else:
+            previous.set_Next(current.get_next())
+
 
 class SkipListNode():
     """
